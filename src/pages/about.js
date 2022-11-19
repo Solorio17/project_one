d76291b611fdcf107829c52c385bd2ef12752e86
Jason <jasonsoloriom@gmail.com>
2022-11-18 22:02:56 -0700

    This will be the about component

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Bear's Cup of Coffee">
+        </Layout>
+    )
+}
+
+export const Head = () => <title>About Us</title>
+
+export default AboutPage;
