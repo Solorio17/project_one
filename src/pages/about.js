6dabc8ae62f2c18b0be9ba4a7e7643beecba0f9a
Jason <jasonsoloriom@gmail.com>
2022-11-20 14:37:10 -0700

    Changed the grid that I had for the menu and moved it to the about page, this is a skeleton of what i envision to become

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,59 @@ import Layout from "../components/layout";
 
 const AboutPage = () => {
     return (
-        <Layout pageTitle="About Bear's Cup of Coffee">
+        <Layout >
+            <section class="hero is-dark">
+                <div className="hero-body">
+                    <p class="title">
+                        About Us
+                    </p>
+                </div>
+            </section>
+
+            <div class="columns is-vcentered">
+                <div class="column is-6" >
+                    <p>Insert a short inspirational quote or a phrase about us</p>
+                </div>
+                <div class="column is-6">
+                    <p>Insert a picture here about something relevant. ex: cup of coffee</p>
+                </div>
+
+            </div>
+            <div class="columns">
+                <div className="column is 2">
+                    Here Insert a color or logo or picture
+                </div>
+                <div class="column is-8">
+                    <h1>Here in the middle is where i want to add about us and how we became to be. our start up story basically</h1>
+                </div>
+                <div class="column is-2">
+                    <p>Same here as the other side a picture logo or phrase</p>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column is-4">
+                    <h1>Customer Testimonial</h1>
+                </div>
+                <div class="column is-4">
+                    <p>Customer Testimonial</p>
+                </div>
+                <div class="column is-4">
+                    <p>Customer Testimonial</p>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column is-6">
+                    <p>Here I can insert maybe like a picture of the store or map</p>
+                </div>
+                <div className="column is-6">
+                    <p>Here I can Insert some sort of information</p>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column is-12">
+                    <p>Here is the footer action</p>
+                </div>
+            </div>
         </Layout>
     )
 }
