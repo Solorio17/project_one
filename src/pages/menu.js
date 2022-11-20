47f32a5245efd33f1f3ec7f7b70ff9416dc2b760
Jason <jasonsoloriom@gmail.com>
2022-11-20 14:39:14 -0700

    the menu is a table and right now has three drinks

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,46 +1,50 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import './mystyles.scss'
 
 const Menu = () => {
     return (
         <Layout>
-            <section class="section">
-                <p class="title">
-                    Our Menu
-                </p>
-                <p class="subtitle">
-                    Hero subtitle
-                </p>
-
-                <div class="columns is-vcentered">
-                    <div class="column is-6" >
-                        <p >COLUMN 6</p>
-                    </div>
-                    <div class="column is-6">
-                        <p >COLUMN 6</p>
-                    </div>
-
+            <section className='hero is-dark'>
+                <div className='hero-body'>
+                    <p className='title'> Our Menu</p>
                 </div>
-                <div class="columns">
-                    <div class="column is-7">
-                        <h1>COLUMN 7</h1>
-                    </div>
-                    <div class="column is-5">
-                        <p>COLUMN 5</p>
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column is-4">
-                        <h1>COLUMN 4</h1>
-                    </div>
-                    <div class="column is-4">
-                        <p>COLUMN 4</p>
-                    </div>
-                    <div class="column is-4">
-                        <p>COLUMN 4</p>
-                    </div>
-                </div>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>SERVING SIZE 12OZ</th>
+                            <th>CALS</th>
+                            <th>FAT</th>
+                            <th>SUGAR</th>
+                            <th>$</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Caramel Machiatto</th>
+                            <td>280</td>
+                            <td>23</td>
+                            <td>22g</td>
+                            <td>4.80</td>
+                        </tr>
+                        <tr>
+                            <th>Iced Matcha Latte</th>
+                            <td>120</td>
+                            <td>12</td>
+                            <td>12g</td>
+                            <td>5.20</td>
+                        </tr>
+                        <tr>
+                            <th>Strawbearry Frappe</th>
+                            <td>400</td>
+                            <td>20</td>
+                            <td>34g</td>
+                            <td>5.80</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
+
         </Layout>
     )
 };
