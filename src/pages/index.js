b6bf25f08c17731ea0f88fd689c411cfafbd9478
Jason <jasonsoloriom@gmail.com>
2022-11-20 14:38:21 -0700

    The home page was empty so I decided to add two hero components. theyre empty right noe

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,27 @@ import Layout from "../components/layout";
 
 const HomePage = () => {
   return (
-    <Layout pageTitle="Bear's Cup of Coffee">
+    <Layout>
+      <section class="hero is-fullheight" style={{ backgroundColor: "wheat" }}>
+        <div class="hero-body">
+          <p class="title">
+            BEAR'S CUP OF COFFEE
+          </p>
+          <p class="subtitle">
+            " Happiness is not something readymade, you must first wait for bear to make it. "
+          </p>
+        </div>
+      </section>
+      <section class="hero is-fullheight" style={{ backgroundColor: "brown" }}>
+        <div class="hero-body">
+          <p class="title">
+            BEAR'S CUP OF COFFEE
+          </p>
+          <p class="subtitle">
+            " Happiness is not something readymade, you must first wait for bear to make it. "
+          </p>
+        </div>
+      </section>
     </Layout>
   )
 }
