6e886cb4be70a1f077eda33538728924726b24ce
Jason <jasonsoloriom@gmail.com>
2022-11-21 02:02:34 -0700

    new page that will show careers

---
+++ src/pages/careers.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const Careers = () => {
+    return (
+        <Layout>
+            <section className='hero is-dark'>
+                <div className='hero-body'>
+                    <p className='title'> Careers With Us</p>
+                </div>
+            </section>
+        </Layout>
+    )
+};
+
+export default Careers;
