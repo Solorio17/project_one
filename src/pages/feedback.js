8304009ce09e231c810f75069d71695964c33479
Jason <jasonsoloriom@gmail.com>
2022-11-28 01:14:01 -0700

    the feedback remains a form for now

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -11,49 +11,49 @@ const Feedback = () => {
             </section>
 
             <section className='section'>
-                <div class="field">
-                    <label class="label">Name</label>
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Name"></input>
+                <div className="field">
+                    <label className="label">Name</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Name"></input>
                     </div>
                 </div>
 
-                <div class="field">
-                    <label class="label">Username</label>
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Your Username"></input>
+                <div className="field">
+                    <label className="label">Username</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Your Username"></input>
                     </div>
                 </div>
 
-                <div class="field">
-                    <label class="label">Email</label>
-                    <div class="control">
-                        <input class="input" type="email" placeholder="Email input"></input>
+                <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control">
+                        <input className="input" type="email" placeholder="Email input"></input>
                     </div>
                 </div>
 
-                <div class="field">
-                    <label class="label">Message</label>
-                    <div class="control">
-                        <textarea class="textarea" placeholder="Your Message..."></textarea>
+                <div className="field">
+                    <label className="label">Message</label>
+                    <div className="control">
+                        <textarea className="textarea" placeholder="Your Message..."></textarea>
                     </div>
                 </div>
 
-                <div class="field">
-                    <div class="control">
-                        <label class="checkbox">
+                <div className="field">
+                    <div className="control">
+                        <label className="checkbox">
                             <input type="checkbox"></input>
                             I agree to the <a href="#">terms and conditions</a>
                         </label>
                     </div>
                 </div>
 
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-link">Submit</button>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-link">Submit</button>
                     </div>
-                    <div class="control">
-                        <button class="button is-link is-light">Cancel</button>
+                    <div className="control">
+                        <button className="button is-link is-light">Cancel</button>
                     </div>
                 </div>
             </section>
