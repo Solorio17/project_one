c71772d81e57d891e2815e3f3ccca21ed5fdd1ab
Jason <jasonsoloriom@gmail.com>
2022-11-29 20:21:06 -0700

    Converted layout into a class based component in order to add state for mobile menu toggle function.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,71 +1,159 @@
 import React, { useState } from 'react'
 import '../pages/mystyles.scss';
 
-const Layout = ({ children }) => {
-    const [isToggleOn, changeToggle] = useState(true);
+class Layout extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div>
-            <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                    <a className="navbar-item" style={{ color: "wheat" }} href="/">
-                        BEAR'S CUP OF COFFEE
-                    </a>
+        this.state = {
+            isToggleOn: true,
+        };
 
-                    <a role="button" className={isToggleOn ? 'navbar-burger is-active' : 'navbar-burger'} aria-expanded="false" onClick={() => changeToggle(!isToggleOn)}>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
+        //the binding is necessary to make 'this' work in the callback (in this example in the onClick)
+        this.changeToggle = this.changeToggle.bind(this)
+    }
 
-                <div className={isToggleOn ? 'navbar-menu is-active' : 'navbar-menu'}>
-                    <div className="navbar-start">
-                        <a className="navbar-item" href='/menu'>
-                            Our Menu
+    changeToggle() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" style={{ color: "wheat" }} href="/">
+                            BEAR'S CUP OF COFFEE
                         </a>
 
-                        <div className="navbar-item has-dropdown is-hoverable">
-                            <a className="navbar-link">
-                                More
+                        <a role="button" className={this.state.isToggleOn ? 'navbar-burger is-active' : 'navbar-burger'} aria-expanded="false" onClick={this.changeToggle}>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </a>
+                    </div>
+
+                    <div className={this.state.isToggleOn ? 'navbar-menu is-active' : 'navbar-menu'}>
+                        <div className="navbar-start">
+                            <a className="navbar-item" href='/menu'>
+                                Our Menu
                             </a>
 
-                            <div className="navbar-dropdown">
-                                <a className="navbar-item" href='/about'>
-                                    About Us
+                            <div className="navbar-item has-dropdown is-hoverable">
+                                <a className="navbar-link">
+                                    More
                                 </a>
-                                <a className="navbar-item" href='/careers'>
-                                    Careers
-                                </a>
-                                <hr className="navbar-divider"></hr>
-                                <a className="navbar-item" href='/feedback'>
-                                    Give us Feedback!
-                                </a>
+
+                                <div className="navbar-dropdown">
+                                    <a className="navbar-item" href='/about'>
+                                        About Us
+                                    </a>
+                                    <a className="navbar-item" href='/careers'>
+                                        Careers
+                                    </a>
+                                    <hr className="navbar-divider"></hr>
+                                    <a className="navbar-item" href='/feedback'>
+                                        Give us Feedback!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="navbar-end">
+                            <div className="navbar-item">
+                                <div className="buttons">
+                                    <a className="button is-small is-dark">
+                                        <strong>Sign up</strong>
+                                    </a>
+                                    <a className="button is-small is-light">
+                                        Log in
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </nav >
 
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <div className="buttons">
-                                <a className="button is-small is-dark">
-                                    <strong>Sign up</strong>
-                                </a>
-                                <a className="button is-small is-light">
-                                    Log in
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav >
-
-            <main>
-                {children}
-            </main>
-        </div >
-    )
+                <main>
+                    {this.props.children}
+                </main>
+            </div >
+        )
+    }
 }
+
+
+
+
+
+
+// const Layout = ({ children }) => {
+//     const [isToggleOn, changeToggle] = useState(true);
+
+//     return (
+//         <div>
+//             <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+//                 <div className="navbar-brand">
+//                     <a className="navbar-item" style={{ color: "wheat" }} href="/">
+//                         BEAR'S CUP OF COFFEE
+//                     </a>
+
+//                     <a role="button" className={isToggleOn ? 'navbar-burger is-active' : 'navbar-burger'} aria-expanded="false" onClick={() => changeToggle(!isToggleOn)}>
+//                         <span aria-hidden="true"></span>
+//                         <span aria-hidden="true"></span>
+//                         <span aria-hidden="true"></span>
+//                     </a>
+//                 </div>
+
+//                 <div className={isToggleOn ? 'navbar-menu is-active' : 'navbar-menu'}>
+//                     <div className="navbar-start">
+//                         <a className="navbar-item" href='/menu'>
+//                             Our Menu
+//                         </a>
+
+//                         <div className="navbar-item has-dropdown is-hoverable">
+//                             <a className="navbar-link">
+//                                 More
+//                             </a>
+
+//                             <div className="navbar-dropdown">
+//                                 <a className="navbar-item" href='/about'>
+//                                     About Us
+//                                 </a>
+//                                 <a className="navbar-item" href='/careers'>
+//                                     Careers
+//                                 </a>
+//                                 <hr className="navbar-divider"></hr>
+//                                 <a className="navbar-item" href='/feedback'>
+//                                     Give us Feedback!
+//                                 </a>
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                     <div className="navbar-end">
+//                         <div className="navbar-item">
+//                             <div className="buttons">
+//                                 <a className="button is-small is-dark">
+//                                     <strong>Sign up</strong>
+//                                 </a>
+//                                 <a className="button is-small is-light">
+//                                     Log in
+//                                 </a>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </nav >
+
+//             <main>
+//                 {children}
+//             </main>
+//         </div >
+//     )
+// }
 
 
 export default Layout
