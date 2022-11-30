8e247e71dc5868ffccb8aeb8ea5da1243f18319c
Jason <jasonsoloriom@gmail.com>
2022-11-29 20:06:57 -0700

    Used something called panels from bulma that imitate almost a searchbar and result tab. Used it to display jobs

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -7,8 +7,54 @@ const Careers = () => {
             <section className='hero is-dark'>
                 <div className='hero-body'>
                     <p className='title'> Careers With Us</p>
+
                 </div>
             </section>
+            <nav class="panel">
+                <p class="panel-heading">
+                    Location: 100 Bear Drive, Cave Avenue, CO 80001
+                </p>
+                <div class="panel-block">
+                    <p class="control has-icons-left">
+                        <input class="input" type="text" placeholder="Search"></input>
+                    </p>
+                </div>
+                <p class="panel-tabs">
+                    <a class="is-active">All</a>
+                    <a>Manager</a>
+                    <a>Crew</a>
+                </p>
+                <a class="panel-block is-active">
+                    <span class="panel-icon">
+                        <i class="fas fa-book" aria-hidden="true"></i>
+                    </span>
+                    Lead Manager (Shift Manager)
+                </a>
+                <a class="panel-block">
+                    <span class="panel-icon">
+                        <i class="fas fa-book" aria-hidden="true"></i>
+                    </span>
+                    Crew Member II (Bearista Specialist)
+                </a>
+                <a class="panel-block">
+                    <span class="panel-icon">
+                        <i class="fas fa-book" aria-hidden="true"></i>
+                    </span>
+                    Crew Member (Training Bearista, Cashier/Order Taker)
+                </a>
+
+                <a class="panel-block">
+                    <span class="panel-icon">
+                        <i class="fas fa-code-branch" aria-hidden="true"></i>
+                    </span>
+                    Janitorial (! Please talk to Manager for this position !)
+                </a>
+                <div class="panel-block">
+                    <button class="button is-link is-outlined is-fullwidth">
+                        Reset all filters
+                    </button>
+                </div>
+            </nav>
         </Layout>
     )
 };
