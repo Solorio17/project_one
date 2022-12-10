835ea60a7f0c21c0628ff98d0cf79d01dd1aeb84
Jason Solorio <jasonsoloriom@gmail.com>
2022-12-10 03:16:17 -0700

    Update menu.js
    crud functionality continued, a bit of a ui upgrade.

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,37 +3,55 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../components/layout';
 import { db } from './index';
-import { collection, getDocs, addDoc, doc, updateDoc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 import './mystyles.scss'
 
 
 const Menu = () => {
     const [isModalOpen, setModalToggle] = useState(true)
+    const [isDropDownOpen, setDropDownOpen] = useState(false)
 
+    // <create drink form state>
     const [newDrink, setNewDrink] = useState("");
     const [newSize, setNewSize] = useState("");
     const [newPrice, setNewPrice] = useState(0);
+    // </create drink form state>
 
     const [drinks, setDrinks] = useState([]);
-    const drinksCollectionRef = collection(db, "drinks")
+    const drinksCollectionRef = collection(db, "drinks");
+
+    // <update drink form state>
+    const [newDrinkName, setNewDrinkName] = useState('');
+    const [newDrinkSize, setNewDrinkSize] = useState(0);
+    const [newDrinkPrice, setNewDrinkPrice] = useState(0);
+    // </update drink form state>
 
 
-    // console.log(isModalOpen);
-
+    // <create drink function>
     const createDrink = async () => {
         await addDoc(drinksCollectionRef, { name: newDrink, size: Number(newSize), price: Number(newPrice) })
     };
+    // </create drink function>
 
-
-
+    // <update drink function>
     const updateDrink = async (id, name, size, price) => {
         const drinkDoc = doc(db, "drinks", id)
         // console.log(name)
         const newFields = { name: name, size: Number(size), price: Number(price) }
         await updateDoc(drinkDoc, newFields)
+        
+        window.location.reload();
     }
+    // </update drink function>
 
+    // <delete drink function>
+    const deleteDrink = async (id) => {
+        const drinkDoc = doc(db, "drinks", id);
+        deleteDoc(drinkDoc)
+    }
+    // </delete drink function>
 
+    // <get drinks function>
     useEffect(() => {
         const getDrinks = async () => {
             const data = await getDocs(drinksCollectionRef);
@@ -42,93 +60,74 @@ const Menu = () => {
         }
         getDrinks()
     }, []);
+    // </get drinks function>
+
 
     return (
         <Layout>
-            <input placeholder="Drink Name..." onChange={(event) => { setNewDrink(event.target.value) }}>
-
-            </input>
-            <input placeholder="Drink Size..." onChange={(event) => { setNewSize(event.target.value) }}></input>
-            <input placeholder="Price" onChange={(event) => { setNewPrice(event.target.value) }}></input>
-
-            <button onClick={createDrink}>Create Drink</button>
-            {drinks.map((drink) => {
-                console.log(drink.id);
-                return (
-                    <div key={drink.id}>
-                        <h1>Name: {drink.name}</h1>
-                        <h1 >Size: {drink.size}oz</h1>
-                        <h1 >Price: ${drink.price}.99</h1>
-                        <h1>ID: {drink.id}</h1>
-                        {/* <button onClick={() => updateDrink(drink.id, drink.price)}>Increase Price</button> */}
-
-                        <button onClick={() => setModalToggle(!isModalOpen)}>Modal</button>
-                        <input placeholder={drink.name} onChange={(event) => { updateDrink(drink.id, event.target.value, drink.size, drink.price) }}></input>
-                        <input placeholder={drink.size} onChange={(event) => { updateDrink(drink.id, drink.name, event.target.value, drink.price) }}></input>
-                        <input placeholder={drink.price} onChange={(event) => { updateDrink(drink.id, drink.name, drink.size, event.target.value) }}></input>
-                        {/* <button onClick={console.log(drink.id, drink.name)}>Info</button> */}
-                        {/* <div className={isModalOpen ? 'modal' : 'modal is-active'} >
-                            <div className="modal-background"></div>
-                            <div className="modal-card">
-                                <header className="modal-card-head">
-                                    <p className="modal-card-title">Modal title</p>
-                                    <button onClick={() => setModalToggle(!isModalOpen)} className="modal-close is-large" aria-label="close"></button>
-
-                                </header>
-                                <section className="modal-card-body">
-                                    <input placeholder={drink.name}></input>
-                                    <input placeholder={drink.size}></input>
-                                    <input placeholder={drink.price}></input>
-                                </section>
-                                <footer className="modal-card-foot">
-                                    <button className="button is-success">Save changes</button>
-                                    <button className="button">Cancel</button>
-                                </footer>
-                            </div>
-                        </div> */}
-                        <hr />
-                    </div>
-                )
-            })}
-
             <section className='hero is-dark'>
                 <div className='hero-body'>
                     <p className='title'> Our Menu</p>
                 </div>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>SERVING SIZE 12OZ</th>
-                            <th>CALS</th>
-                            <th>FAT</th>
-                            <th>SUGAR</th>
-                            <th>$</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>Caramel Machiatto</th>
-                            <td>280</td>
-                            <td>23</td>
-                            <td>22g</td>
-                            <td>4.80</td>
-                        </tr>
-                        <tr>
-                            <th>Iced Matcha Latte</th>
-                            <td>120</td>
-                            <td>12</td>
-                            <td>12g</td>
-                            <td>5.20</td>
-                        </tr>
-                        <tr>
-                            <th>Strawbearry Frappe</th>
-                            <td>400</td>
-                            <td>20</td>
-                            <td>34g</td>
-                            <td>5.80</td>
-                        </tr>
-                    </tbody>
-                </table>
+            
+                <div className={isModalOpen ? 'modal' : 'modal is-active'} >
+                    <div className="modal-background"></div>
+                    <div className="modal-card">
+                        <header className="modal-card-head">
+                            <p className="modal-card-title">Modal title</p>
+                        </header>
+                        <section className="modal-card-body">
+                            <input className='input is-rounded' placeholder="Drink Name..." onChange={(event) => { setNewDrink(event.target.value) }}></input>
+                            <input type='number' className='input is-rounded' placeholder="Drink Size..." onChange={(event) => { setNewSize(event.target.value) }}></input>
+                            <input type='number' className='input is-rounded' placeholder="Price $" onChange={(event) => { setNewPrice(event.target.value) }}></input>
+                        </section>
+                        <footer className="modal-card-foot">
+                            <button className='button is-warning' onClick={createDrink}>Create Drink</button>
+                            <button className="button is-danger"  onClick={() => setModalToggle(!isModalOpen)}>Cancel</button>
+                        </footer>
+                    </div>
+                </div>
+                <button className='button is-primary' onClick={() => setModalToggle(!isModalOpen)}>Create New Drink</button>
+                <hr className="dropdown-divider"></hr>
+
+
+            {drinks.map((drink) => {
+                return (
+                    <section className='hero' key={drink.id}>
+                        <table className='table' >
+                            <thead>
+                                <tr>
+                                    <th>SERVING SIZE: {drink.size}oz</th>
+                                    <th>CALS</th>
+                                    <th>FAT</th>
+                                    <th>SUGAR</th>
+                                    <th>$</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>{drink.name}</th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th>{drink.price}</th>
+                                </tr>
+   
+                            </tbody>
+                        
+                        </table>
+
+                        <button className="button is-info" onClick={() => setDropDownOpen(!isDropDownOpen)} >Edit Drink </button>
+                        <button className='button is-danger' onClick={() => deleteDrink(drink.id)}>Delete</button>
+                        <input className='input' defaultValue={drink.name} onChange={(event) => { setNewDrinkName(event.target.value)}}></input>
+                        <input className='input' type='number' defaultValue={drink.size} onChange={(event) => { setNewDrinkSize(event.target.value)}}></input>
+                        <input className='input' type='number' defaultValue={drink.price} onChange={(event) => { setNewDrinkPrice(event.target.value)}}></input>
+                        <button className='button is-link' onClick={() => {updateDrink(drink.id, newDrinkName, newDrinkSize, newDrinkPrice)}}>Save Changes</button>
+                        <hr className="dropdown-divider"></hr>
+                        <hr className="dropdown-divider"></hr>
+                    </section>
+                )
+            })}
             </section>
         </Layout >
     )
