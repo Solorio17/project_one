686363c94bde952aa8e026b4e885ccd0446ac6a7
Jason Solorio <jasonsoloriom@gmail.com>
2022-12-15 22:26:58 -0700

    About page redesign con't

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout";
 import './mystyles.scss';
 import LongImage from '../images/longimage.jpg';
@@ -8,6 +8,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faMoon, faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons'
 
 const AboutPage = () => {
+
+    const [ourStoryIsOpen, setOurStoryIsOpen] = useState(false);
+    const [ajsBeansIsOpen, setAjsBeansIsOpen] = useState(false);
+    const [givebackIsOpen, setGivebackIsOpen] = useState(false);
+
+    const ourStoryToggle = () => {
+        setOurStoryIsOpen(!ourStoryIsOpen)
+    }
+    const ajsBeansToggle = () => {
+        setAjsBeansIsOpen(!ajsBeansIsOpen)
+    }
+    const givebackToggle = () => {
+        setGivebackIsOpen(!givebackIsOpen)
+    }
+
     return (
         <Layout>
             <section className="hero is-dark" name="top">
@@ -35,7 +50,7 @@ const AboutPage = () => {
                             <div className="box myNotification">
                                 <FontAwesomeIcon icon={faCoffee} size="10x" />
                             </div>
-                            <button className="button is-small is-info">Read More</button>
+                            <button className="button is-small is-dark myBtn" onClick={ourStoryToggle}>Read More</button>
                         </div>
                     </div>
                     <div class="column">
@@ -44,7 +59,7 @@ const AboutPage = () => {
                             <div className="box myNotification">
                                 <FontAwesomeIcon icon={faMoon} size="10x" />
                             </div>
-                            <button className="button is-small is-danger">Read More</button>
+                            <button className="button is-small is-dark myBtn" onClick={ajsBeansToggle}>Read More</button>
                         </div>
                     </div>
                     <div class="column">
@@ -54,7 +69,7 @@ const AboutPage = () => {
                                 <FontAwesomeIcon icon={faHandHoldingHeart} size="10x" />
 
                             </div>
-                            <button className="button is-small is-warning">Read More</button>
+                            <button className="button is-small is-dark myBtn" onClick={givebackToggle}>Read More</button>
                         </div>
                     </div>
                 </div>
@@ -132,8 +147,85 @@ const AboutPage = () => {
                         </div>
                     </div>
                 </div>
-
             </div>
+
+
+            <div className={ourStoryIsOpen ? 'modal is-active' : 'modal'}>
+                <div class="modal-background ourstoryModal"></div>
+                <section className="hero is-large">
+                    <div className="modal-content">
+                        <div className="hero-body ourstoryNotif">
+                            <h1 className="title">About Bears Cup of Coffee</h1>
+                            <p>
+                                Meoooow cough furball into food bowl then scratch owner for a new one. Cats are a queer kind of folk relentlessly pursues moth. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Sleeps on my head mark territory. Paw at beetle and eat it before it gets away i'm going to lap some water out of my master's cup meow, lick human with sandpaper tongue scratch lick sellotape, refuse to come home when humans are going to bed;
+                            </p>
+                            <hr></hr>
+                            <h1 className="title">About Bears Cup of Coffee</h1>
+                            <p>
+                                Meoooow cough furball into food bowl then scratch owner for a new one. Cats are a queer kind of folk relentlessly pursues moth. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Sleeps on my head mark territory. Paw at beetle and eat it before it gets away i'm going to lap some water out of my master's cup meow, lick human with sandpaper tongue scratch lick sellotape, refuse to come home when humans are going to bed;
+                            </p>
+                            <hr></hr>
+                            <h1 className="title">About Bears Cup of Coffee</h1>
+                            <p>
+                                Meoooow cough furball into food bowl then scratch owner for a new one. Cats are a queer kind of folk relentlessly pursues moth. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Sleeps on my head mark territory. Paw at beetle and eat it before it gets away i'm going to lap some water out of my master's cup meow, lick human with sandpaper tongue scratch lick sellotape, refuse to come home when humans are going to bed;
+                            </p>
+                        </div>
+                    </div>
+
+                </section>
+                <button class="modal-close is-large" aria-label="close" onClick={ourStoryToggle}></button>
+            </div>
+            <div className={ajsBeansIsOpen ? 'modal is-active' : 'modal'}>
+                <div class="modal-background ajsbeansModal"></div>
+                <div class="modal-content">
+                    <section className="hero is-large">
+                        <div className="notification ajsbeansModal">
+                            <div className="tile is-ancestor">
+                                <div className="tile is-parent">
+                                    <div className="tile is-child notification ajsbeansNotif">
+                                        <h1 className="title">A&J's Beans</h1>
+                                        <p>
+                                            Meoooow cough furball into food bowl then scratch owner for a new one. Cats are a queer kind of folk relentlessly pursues moth. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Sleeps on my head mark territory. Paw at beetle and eat it before it gets away i'm going to lap some water out of my master's cup meow, lick human with sandpaper tongue scratch lick sellotape, refuse to come home when humans are going to bed;
+                                        </p>
+                                        <hr></hr>
+                                        <h1 className="title">A&J's Beans</h1>
+                                        <p>
+                                            Meoooow cough furball into food bowl then scratch owner for a new one. Cats are a queer kind of folk relentlessly pursues moth. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Sleeps on my head mark territory. Paw at beetle and eat it before it gets away i'm going to lap some water out of my master's cup meow, lick human with sandpaper tongue scratch lick sellotape, refuse to come home when humans are going to bed;
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <button class="modal-close is-large" aria-label="close" onClick={ajsBeansToggle}></button>
+            </div>
+            <div className={givebackIsOpen ? 'modal is-active' : 'modal'}>
+                <div class="modal-background givebackModal"></div>
+                <div class="modal-content">
+                    <section className="hero is-large">
+                        <div className="notification givebackModal">
+                            <div className="tile is-ancestor">
+                                <div className="tile is-parent">
+                                    <div className="tile is-child notification givebackNotif">
+                                        <h1 className="title">How we Give Back to our Earth!</h1>
+                                        <p>
+                                            Meoooow cough furball into food bowl then scratch owner for a new one. Cats are a queer kind of folk relentlessly pursues moth. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Sleeps on my head mark territory. Paw at beetle and eat it before it gets away i'm going to lap some water out of my master's cup meow, lick human with sandpaper tongue scratch lick sellotape, refuse to come home when humans are going to bed;
+                                        </p>
+                                        <hr></hr>
+                                        <h1 className="title">How we Give Back to our Earth!</h1>
+                                        <p>
+                                            Meoooow cough furball into food bowl then scratch owner for a new one. Cats are a queer kind of folk relentlessly pursues moth. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Sleeps on my head mark territory. Paw at beetle and eat it before it gets away i'm going to lap some water out of my master's cup meow, lick human with sandpaper tongue scratch lick sellotape, refuse to come home when humans are going to bed;
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <button class="modal-close is-large" aria-label="close" onClick={givebackToggle}></button>
+            </div>
+
         </Layout>
     )
 }
